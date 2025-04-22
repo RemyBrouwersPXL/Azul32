cfd50dd556228371713f912af14cca3ba35a8225
PieterDugardynPXL <pieter.dugardyn@student.pxl.be>
2025-04-22 10:55:39 +0200

    lobby.js 0.3

--- Frontend/js/lobby.js
+++ Frontend/js/lobby.js
@@ -1,16 +1,14 @@
 document.getElementById('gameMode').addEventListener('click', function () {
     // Show loader and overlay
     document.getElementById('loaderOverlay').style.display = 'flex';
+    document.getElementById('gameMode').style.display = 'none';
+    document.getElementById('loading_msg').style.display = 'unset';
 
-    // Darken the background (optional - if you want to darken the body content too)
+    // Darken the background
     document.body.style.filter = 'brightness(0.7)';
 
     // You can add your game loading logic here
-    gameModeDiv.addEventListener("click", function () {
 
-        gameModeDiv.hidden = true;
-        loaderOverlay.hidden = false;
-    }
     // Example: After 5 seconds, hide the loader (remove this in production)
     // setTimeout(function() {
     //     document.getElementById('loaderOverlay').style.display = 'none';
