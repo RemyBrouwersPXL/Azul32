04f093cbad51f16aa755192fd18e5739b34607e0
RemyBrouwersPXL <remy.brouwers@student.pxl.be>
2025-05-14 10:40:53 +0200

    game-over

---
+++ Frontend/js/game-over.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", async function() {
+    try {
+        const urlParams = new URLSearchParams(window.location.search);
+        const gameId = urlParams.get('gameId');
+        const token = sessionStorage.getItem('userToken');
+
+        const gameRes = await fetch(`https://localhost:5051/api/Games/${gameId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+                'Accept': 'text/plain'
+            }
+        });
+        if (!gameRes.ok) throw new Error('Failed to fetch game data');
+        const data = await gameRes.json();
+
+        if (!data.hasEnded) return;
+
+        const podium = document.getElementById('podium');
+
+        const players = data.players.map(p => ({
+            name: p.name,
+            score: p.board.score
+        }));
+
+
+        players.sort((a, b) => b.score - a.score);
+
+        players.forEach((player, index) => {
+            const div = document.createElement('div');
+            div.classList.add('podium-player');
+
+            if (index === 0) div.classList.add('podium-1');
+            else if (index === 1) div.classList.add('podium-2');
+            else if (index === 2) div.classList.add('podium-3');
+            else div.style.height = `${100 - index * 10}px`; // lagere spelers
+
+            div.innerHTML = `
+                <h2>${index + 1}e: ${player.name}</h2>
+                <div class="score">${player.score} punten</div>
+                `;
+            podium.appendChild(div);
+        });
+    } catch (e) {
+        console.error('Polling error:', e);
+    }
+    
+       
+});
